125ea076d92ba81d014b2f269bcb136220f1c724
SO HUI <haejuuun925@gmail.com>
2024-02-10 23:06:07 +0900

    Feat: node styling 및 antd install

--- src/components/Node/Node.tsx
+++ src/components/Node/Node.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Modal from "../Modal/Modal";
+import * as S from "./Node.style";
 
 const Node = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -18,23 +19,20 @@ const Node = () => {
   }
 
   return (
-    <>
-      <div
-        className="node"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        하이
-        {isHovered && (
-          <>
-            <button className="plusBtn" onClick={showModal}>+</button>
-            {modalOpen && <Modal setModalOpen={setModalOpen} title='질문 추가' /> }
-            <button className="detailBtn">자세히 보기</button>
-          </>
-        )}
-      </div>
-    </>
-    
+    <S.Wrapper>
+      <S.DetailBtn className={isHovered ? "show" : ""}>자세히</S.DetailBtn>
+      <S.NodeAndAddBtnWrapper> 
+        <S.NodeAddBtn className={isHovered ? "show" : ""} />
+        <S.NodeBox
+          className="node"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          노드            
+        </S.NodeBox>
+        <S.NodeAddBtn className={isHovered ? "show" : ""} />
+      </S.NodeAndAddBtnWrapper>
+    </S.Wrapper>
   )
 };
 

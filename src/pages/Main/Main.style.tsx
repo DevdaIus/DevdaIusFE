2e4820de0494c654acd4370a020e914555f16ef8
Raon <raonharu0822@kakao.com>
2024-02-15 12:20:05 +0900

    Design: 메인 배경 수정

--- src/pages/Main/Main.style.tsx
+++ src/pages/Main/Main.style.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import mainbg from "../../assets/mainbg.svg";
 
 export const Wrap = styled.div``;
 
@@ -7,12 +8,21 @@ export const MainContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background: url(${mainbg});
+  width: 1240px;
+  height: 682px;
+  margin: 0 auto;
+  margin-top: 136px;
+  margin-bottom: 136px;
 `;
 
-export const Logo = styled.h2``;
+export const Logo = styled.img`
+  margin-bottom: 15px;
+`;
 export const Desc = styled.p`
   font-size: 15px;
   color: #505b8c;
+  margin-bottom: 45px;
 `;
 export const TitleForm = styled.form`
   display: flex;
@@ -20,7 +30,7 @@ export const TitleForm = styled.form`
 `;
 export const TitleInput = styled.input`
   width: 438px;
-  height: 25px;
+  height: 55px;
   border: 2px solid #687aa5;
   border-radius: 50px;
   padding: 14.5px 20px;

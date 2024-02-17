9bec74e4c285d1c658778441d900d5ee546020ad
SO HUI <haejuuun925@gmail.com>
2024-02-17 16:17:02 +0900

    Feat: node에 MoreModal open 연결

--- src/components/Node/Node.tsx
+++ src/components/Node/Node.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Modal from "../Modal/Modal";
 import * as S from "./Node.style";
+import MoreModal from "../MoreModal/MoreModal";
 
 const Node = () => {
   const [isHovered, setIsHovered] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
+  const [moreModalOpen, setMoreModalOpen] = useState(false);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -18,12 +20,16 @@ const Node = () => {
     setModalOpen(true);
   };
 
+  const onShowMoreModal = () => {
+    setMoreModalOpen(true);
+  }
+
   return (
     <S.Wrapper 
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <S.DetailBtn className={isHovered ? "show" : ""}>자세히</S.DetailBtn>
+      <S.DetailBtn className={isHovered ? "show" : ""} onClick={onShowMoreModal} >자세히</S.DetailBtn>
       <S.NodeAndAddBtnWrapper> 
         <S.NodeAddBtn className={isHovered ? "show" : ""} onClick={onShowModal} />
         <S.NodeBox
@@ -34,12 +40,22 @@ const Node = () => {
           노드            
         </S.NodeBox>
         <S.NodeAddBtn className={isHovered ? "show" : ""} onClick={onShowModal} />
-        {modalOpen && (<Modal
-          open={modalOpen}
-          onClose={() => {
-          setModalOpen(false);
-        }}
-      />)}
+        {modalOpen && (
+          <Modal
+            open={modalOpen}
+            onClose={() => {
+              setModalOpen(false);
+            }}
+          />
+        )}
+        {moreModalOpen && (
+          <MoreModal
+            open={modalOpen}
+            onClose={() => {
+              setMoreModalOpen(false);
+            }}
+          />
+        )}
       </S.NodeAndAddBtnWrapper>
     </S.Wrapper>
   )

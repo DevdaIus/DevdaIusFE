9832bd4e1875050a07cf3ddf5f409bda1428cbb0
SO HUI <haejuuun925@gmail.com>
2024-02-29 17:11:16 +0900

    Feat: 자세히 모달창 퍼블리싱 및 클립보드 copy hook 구현

--- src/components/MoreModal/MoreModal.style.tsx
+++ src/components/MoreModal/MoreModal.style.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import theme from "./../../style/theme";
-import { CloseOutlined } from "@ant-design/icons";
+import { CheckCircleOutlined, CloseOutlined, CopyOutlined, FormOutlined } from "@ant-design/icons";
 
 export const Overlay = styled.div`
   position: fixed;
@@ -15,8 +15,8 @@ export const Overlay = styled.div`
 `;
 
 export const ModalWrap = styled.div`
-  width: 569px;
-  height: 341px;
+  width: 779px;
+  height: 489px;
   border-radius: 15px;
   background-color: #fff;
   position: absolute;
@@ -41,21 +41,58 @@ export const QuesAddTitle = styled.p`
   font-style: ${theme.fonts.S_Core_Dream};
   color: ${theme.colors.G4};
   font-size: 13px;
-  margin-left: 230px;
-  margin-right: 230px;
+  margin-left: 340px;
+  margin-right: 340px;
 `;
 
 export const CloseBtn = styled(CloseOutlined)`
   cursor: pointer;
   color: ${theme.colors.G4};
-  float: right;
+`;
+
+export const AnsInput = styled.textarea`
+  width: 590px;
+  height: 123px;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 15px;
+  background: ${theme.colors.G1};
+  color: ${theme.colors.G4};
+  border: 1px solid ${theme.colors.G2};
+  box-shadow: inset 0px 1px 8px rgba(0, 0, 0, 0.2);
+  font-style: ${theme.fonts.S_Core_Dream};
+  font-size: 13px;
+  outline: none;
+  padding-left: 10px;
+  -webkit-transition: 0.18s ease-out;
+  -moz-transition: 0.18s ease-out;
+  -o-transition: 0.18s ease-out;
+  transition: 0.18s ease-out;
+  resize: none;
+
+  &::placeholder {
+    color: ${theme.colors.G4};
+    font-style: ${theme.fonts.S_Core_Dream};
+    font-size: 13px;
+  }
+
+  &:hover {
+    box-shadow: inset 1px 2px 8px rgba(0, 0, 0, 0.02);
+  }
+
+  &:focus {
+    color: #4b515d;
+    border: 1px solid #b8b6b6;
+    box-shadow: inset 1px 2px 4px rgba(0, 0, 0, 0.01),
+      0px 0px 8px rgba(0, 0, 0, 0.2);
+  }
 `;
 
 export const QuesInput = styled.textarea`
-  width: 482px;
-  height: 184px;
-  margin-top: 15px;
-  margin-bottom: 15px;
+  width: 675px;
+  height: 123px;
+  margin-bottom: 10px;
+  padding: 10px;
   border-radius: 15px;
   background: ${theme.colors.G1};
   color: ${theme.colors.G4};
@@ -90,8 +127,8 @@ export const QuesInput = styled.textarea`
 `;
 
 export const ConfirmBtn = styled.button`
-  width: 482px;
-  height: 49px;
+  width: 75px;
+  height: 123px;
   border-radius: 15px;
   background-color: ${theme.colors.MainColor};
   color: ${theme.colors.W};
@@ -99,4 +136,73 @@ export const ConfirmBtn = styled.button`
   font-size: 13px;
   text-align: center;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+  margin-left: 10px;
+`;
+
+export const QAText = styled.text`
+  font-style: ${theme.fonts.S_Core_Dream};
+  color: ${theme.colors.MainColor};
+  font-weight: bold;
+  margin-right: 10px;
+`;
+
+export const QuesBox = styled.div`
+  width: 675px;
+  height: 123px;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 15px;
+  background: ${theme.colors.G1};
+  color: ${theme.colors.G4};
+  border: 1px solid ${theme.colors.G2};
+  box-shadow: inset 0px 1px 8px rgba(0, 0, 0, 0.2);
+  font-style: ${theme.fonts.S_Core_Dream};
+  font-size: 13px;
+  text-align: left;
+`
+
+export const QuesAnsWrap = styled.div`
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const QuesWrap = styled.div`
+  display: flex;
+  margin-top: 150px;
+`;
+
+export const AnsWrap = styled.div`
+  display: flex;
+  margin-top: 10px;
+`;
+
+export const EditBtn = styled(FormOutlined)`
+  cursor: pointer;
+  color: ${theme.colors.B};
+  margin-right: 7px;
+`;
+
+export const CopyBtn = styled(CopyOutlined)`
+  cursor: pointer;
+  color: ${theme.colors.B};
+`;
+
+export const BtnWrap = styled.div`
+  display: flex;
+  position: absolute;
+  right: 14px;
+  bottom: 160px;
+`;
+
+export const SaveBtn = styled(CheckCircleOutlined)`
+  cursor: pointer;
+  color: ${theme.colors.B};
+  margin-right: 7px;
+`;
+
+export const CancelBtn = styled(CloseOutlined)`
+  cursor: pointer;
+  color: ${theme.colors.B};
 `;
